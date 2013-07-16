fcf26d41ca06e735eddeda69257fae23187de0ab
Mark Constable <markc@renta.net>
2013-07-16 14:12:09 +1000

    simplelogin: revised users table, removed create_session(), added admin[] to cfg()

--- public/simplelogin.php
+++ public/simplelogin.php
@@ -20,30 +20,37 @@ if (version_compare(PHP_VERSION, '5.5.0', '<')) {
 }
 
 session_start();
-//error_log('GET='.var_export($_GET, true));
-//error_log('POST='.var_export($_POST, true));
-//error_log('SESSION='.var_export($_SESSION, true));
+error_log('GET='.var_export($_GET, true));
+error_log('POST='.var_export($_POST, true));
+error_log('SESSION='.var_export($_SESSION, true));
 
 echo view(init(cfg(array(
-    'title'     => 'Simple PHP Login',
-    'admin'     => 'admin',
-    'email'     => 'admin@localhost.lan',
-    'passwd'    => 'changeme',
-    'db'        => null,
-    'dbconf'    => array(
-        'host'  => 'localhost',
-        'name'  => 'users',
-        'pass'  => 'changeme',
-        'path'  => 'users.db',
-        'port'  => '3306',
-        'type'  => 'sqlite',
-        'user'  => 'root'),
-    'views'     => array(
+    'title'         => 'Simple PHP Login',
+    'db'            => null,
+    'dbconf'        => array(
+        'host'      => 'localhost',
+        'name'      => 'users',
+        'pass'      => 'changeme',
+        'path'      => 'users.db',
+        'port'      => '3306',
+        'type'      => 'mysql',
+        'user'      => 'root'),
+    'admin'         => array(
+        'id'        => '1000',
+        'uid'       => 'admin',
+        'fname'     => 'System',
+        'lname'     => 'Administrator',
+        'email'     => 'admin@localhost.lan',
+        'passwd'    => 'changeme',
+        'updated'   => '',
+        'created'   => ''),
+    'views'         => array(
         'home',
         'about',
         'login',
         'logout',
         'register',
+        'forgotpw',
         'profile',
         'install'
     )))));
@@ -54,16 +61,28 @@ function init($cfg)
     $a = $_SESSION['a'] = isset($_REQUEST['a'])
         ? strtolower(str_replace(' ', '_', trim($_REQUEST['a'])))
         : 'home';
-    return in_array($a, $cfg['views'])? $a(): '<b>View does not exist</b>';
+    return in_array($a, $cfg['views']) ? $a() : '<b>View does not exist</b>';
 }
 
 // public callable functions
 
 function home()
 {
-    return isset($_SESSION['user_logged_in']) ? '
+    return '
+    <p>TODO: This is the Home page.</p>';
+}
+
+function about()
+{
+    return '
+    <p>TODO: This is the About page.</p>';
+}
+
+function profile()
+{
+    return isset($_SESSION['u']) ? '
     <p>
-      Hello, '.$_SESSION['user_name'].'. You are now logged in. Try to close
+      Hello, '.$_SESSION['u']['name'].'. You are now logged in. Try to close
       this browser tab and open it again. Still logged in! ;)
     </p>
     <p>
@@ -73,8 +92,8 @@ function home()
 
 function logout()
 {
-    $_SESSION = array();
-    $_SESSION['msg'] = "You are now logged out";
+    unset($_SESSION['u']);
+    $_SESSION['m'] = "You are now logged out";
     header('Location: '.$_SERVER['PHP_SELF']);
     exit();
 }
@@ -82,20 +101,16 @@ function logout()
 function login()
 {
     if (!empty($_POST)) {
-        $msg = '';
-        if (strlen($_POST['user_name']) > 1) {
-            if (strlen($_POST['user_password']) > 5) {
-                $user = read_user($_POST['user_name']);
-                if (isset($user['user_name'])) {
-                    if (password_verify($_POST['user_password'], $user['user_password_hash'])) {
-                        create_session($user);
-                        header('Location: '.$_SERVER['PHP_SELF']);
-                        exit();
-                    } else $msg = 'Passwords do not match';
-                } else $msg = 'Username does not exist';
-            } else $msg = 'Password must be at least 6 characters';
-        } else $msg = 'Username must be at least 2 characters';
-        $_SESSION['msg'] = $msg;
+        $m = '';
+        $u = read_user($_POST['u']['uid']);
+        if (isset($u['uid'])) {
+            if (password_verify($_POST['u']['passwd'], $u['passwd'])) {
+                $_SESSION['u'] = $u;
+                header('Location: '.$_SERVER['PHP_SELF']);
+                exit();
+            } else $m = 'Passwords do not match';
+        } else $m = 'Username does not exist';
+        $_SESSION['m'] = $m;
     }
     return login_form();
 }
@@ -103,7 +118,7 @@ function login()
 function register()
 {
     if (!empty($_POST)) {
-        $msg = '';
+        $m = '';
         if ($_POST['user_name']) {
             if ($_POST['user_password_new']) {
                 if ($_POST['user_password_new'] === $_POST['user_password_repeat']) {
@@ -116,20 +131,20 @@ function register()
                                         if (strlen($_POST['user_email']) < 65) {
                                             if (filter_var($_POST['user_email'], FILTER_VALIDATE_EMAIL)) {
                                                 create_user();
-                                                $_SESSION['msg'] = 'You are now registered so please login';
+                                                $_SESSION['m'] = 'You are now registered so please login';
                                                 header('Location: '.$_SERVER['PHP_SELF']);
                                                 exit();
-                                            } else $msg = 'You must provide a valid email address';
-                                        } else $msg = 'Email must be less than 64 characters';
-                                    } else $msg = 'Email cannot be empty';
-                                } else $msg = 'Username already exists';
-                            } else $msg = 'Username must be only a-z, A-Z, 0-9';
-                        } else $msg = 'Username must be between 2 and 64 characters';
-                    } else $msg = 'Password must be at least 6 characters';
-                } else $msg = 'Passwords do not match';
-            } else $msg = 'Empty Password';
-        } else $msg = 'Empty Username';
-        $_SESSION['msg'] = $msg;
+                                            } else $m = 'You must provide a valid email address';
+                                        } else $m = 'Email must be less than 64 characters';
+                                    } else $m = 'Email cannot be empty';
+                                } else $m = 'Username already exists';
+                            } else $m = 'Username must be only a-z, A-Z, 0-9';
+                        } else $m = 'Username must be between 2 and 64 characters';
+                    } else $m = 'Password must be at least 6 characters';
+                } else $m = 'Passwords do not match';
+            } else $m = 'Empty Password';
+        } else $m = 'Empty Username';
+        $_SESSION['m'] = $m;
     }
     return register_form();
 }
@@ -142,29 +157,28 @@ function install()
         ? 'int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT'
         : 'INTEGER PRIMARY KEY';
     $ind = $dbc['type'] === 'mysql'
-        ? ' ALTER TABLE `users` ADD UNIQUE (`user_name`);'
-        : 'CREATE UNIQUE INDEX `user_name_UNIQUE` ON `users` (`user_name` ASC);';
+        ? 'ALTER TABLE `uid` ADD UNIQUE (`uid`); ALTER TABLE `email` ADD UNIQUE (`email`);'
+        : 'CREATE UNIQUE INDEX `uid_UNIQUE` ON `users` (`uid` ASC); CREATE UNIQUE INDEX `email_UNIQUE` ON `users` (`email` ASC);';
 
     // uncomment below to reinstall tables while testing
-    //$dbh->exec("DROP TABLE IF EXISTS `users`;");
+    $dbh->exec("DROP TABLE IF EXISTS `users`;");
 
     try {
         $dbh->exec("
  CREATE TABLE IF NOT EXISTS `users` (
-        `user_id` $pri,
-        `user_name` varchar(64),
-        `user_password_hash` varchar(255),
-        `user_email` varchar(64));
- $ind");
-    } catch (PDOException $e) {
-        die($e->getMessage());
-    }
-    $_POST['user_name'] = cfg('admin');
-    $_POST['user_email'] = cfg('email');
-    $_POST['user_password_new'] = cfg('passwd');
-    create_user();
-    $_SESSION = array();
-    $_SESSION['msg'] = 'Database and default user are now installed, please login';
+        `id` $pri,
+        `uid` varchar(63),
+        `fname` varchar(31),
+        `lname` varchar(31),
+        `email` varchar(63),
+        `passwd` varchar(255),
+        `updated` datetime,
+        `created` datetime
+); $ind");
+    } catch (PDOException $e) { die($e->getMessage()); }
+    create_user(cfg('admin'));
+    unset($_SESSION['u']);
+    $_SESSION['m'] = 'Database and default user are now installed, please login';
     header('Location: '.$_SERVER['PHP_SELF']);
     exit();
 }
@@ -182,9 +196,9 @@ function cfg($k = NULL, $v = NULL)
 
 function view($content)
 {
-    $msg = isset($_SESSION['msg']) ? '
-    <p class="msg">'.$_SESSION['msg'].'</p>' : '';
-    unset($_SESSION['msg']);
+    $m = isset($_SESSION['m']) ? '
+    <p class="m">'.$_SESSION['m'].'</p>' : '';
+    unset($_SESSION['m']);
 
     return '<!DOCTYPE html>
 <html lang="en">
@@ -194,17 +208,17 @@ function view($content)
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <style>
 body { margin: 0 auto; width: 42em; }
-h1, form, .msg { text-align: center; }
+h1, form, .m { text-align: center; }
 label { display: inline-block; width: 10em; text-align: right;  }
 a { text-decoration: none; }
-.msg, .btn { border: 1px solid #CFCFCF; padding: 0.25em 1em 0.5em 1em; border-radius: 0.3em; }
-.msg { background-color: #FFEFEF; color: #DF0000; font-weight: bold; }
-.btn { background-color: #EFEFEF; }
+.m, .btn { border: 1px solid #CFCFCF; padding: 0.25em 1em 0.5em 1em; border-radius: 0.3em; }
+.m { background-color: #FFEFEF; color: #DF0000; font-weight: bold; }
+.btn { background-color: #EFEFEF; font-size: 75%; }
 .btn:hover { background-color: #DFDFDF; }
     </style>
   </head>
   <body>
-    <h1>'.cfg('title').'</h1>'.$msg.$content.'
+    <h1>'.cfg('title').'</h1>'.$m.$content.'
   </body>
 </html>
 ';
@@ -212,17 +226,17 @@ a { text-decoration: none; }
 
 function login_form()
 {
-    $user_name = isset($_POST['user_name']) ? $_POST['user_name'] : '';
+    $uid = isset($_POST['u']['uid']) ? $_POST['u']['uid'] : '';
     return '
-    <form method="post" action="?a=login" name="loginform">
-      <label for="login_input_username">Username</label>
-      <input id="login_input_username" class="login_input" type="text" name="user_name" value="'.$user_name.'" required>
+    <form method="post" action="?a=login">
+      <label>Login ID</label>
+      <input type="text" name="u[uid]" value="' . $uid . '" required>
       <br>
-      <label for="login_input_password">Password</label>
-      <input id="login_input_password" class="login_input" type="password" name="user_password" autocomplete="off" required>
+      <label>Password</label>
+      <input type="password" name="u[passwd]" autocomplete="off" required>
       <br>
       <br>
-      <input type="submit"  name="a" value="Login" />
+      <input type="submit" value="Login" />
       <br>
       <br>
       <a class="btn" href="?a=register">Register New Account</a>
@@ -232,28 +246,34 @@ function login_form()
 
 function register_form()
 {
-    $user_name = isset($_POST['user_name']) ? $_POST['user_name'] : '';
-    $user_email = isset($_POST['user_email']) ? $_POST['user_email'] : '';
+    $uid = isset($_POST['u']['uid']) ? $_POST['u']['uid'] : '';
+    $fname = isset($_POST['u']['fname']) ? $_POST['u']['fname'] : '';
+    $lname = isset($_POST['u']['lname']) ? $_POST['u']['lname'] : '';
+    $email = isset($_POST['u']['email']) ? $_POST['u']['email'] : '';
     return '
-    <form method="post" action="?a=register" name="registerform">
+    <form method="post" action="?a=register">
       <p>All fields are required. Username must be only letters and numbers from<br>
       2 to 64 characters long and the password has to be at least 6 characters.</p>
-      <!-- the user name input field uses a HTML5 pattern check -->
-      <label for="login_input_username">Username</label>
-      <input id="login_input_username" class="login_input" type="text" pattern="[a-zA-Z0-9]{2,64}" name="user_name" value="'.$user_name.'" required>
+      <label>Username</label>
+      <input type="text" pattern="[a-zA-Z0-9]{2,64}" name="u[uid]" value="' . $uid . '" required>
       <br>
-      <!-- the email input field uses a HTML5 email type check -->
-      <label for="login_input_email">Email Address</label>
-      <input id="login_input_email" class="login_input" type="email" name="user_email" value="'.$user_email.'" required>
+      <label>First Name</label>
+      <input type="text" pattern="[a-zA-Z0-9]{2,32}" name="u[fname]" value="' . $fname . '" required>
       <br>
-      <label for="login_input_password_new">Password</label>
-      <input id="login_input_password_new" class="login_input" type="password" name="user_password_new" pattern=".{6,}" required autocomplete="off">
+      <label>Last Name</label>
+      <input type="text" pattern="[a-zA-Z0-9]{2,32}" name="u[lname]" value="' . $lname . '" required>
       <br>
-      <label for="login_input_password_repeat">Confirm Password</label>
-      <input id="login_input_password_repeat" class="login_input" type="password" name="user_password_repeat" pattern=".{6,}" required autocomplete="off">
+      <label>Email Address</label>
+      <input type="email" name="u[email]" value="'.$email.'" required>
+      <br>
+      <label>Password</label>
+      <input type="password" name="u[pass1]" pattern=".{6,}" required autocomplete="off">
+      <br>
+      <label>Confirm Password</label>
+      <input type="password" name="u[pass2]" pattern=".{6,}" required autocomplete="off">
       <br>
       <br>
-      <input type="submit"  name="a" value="Register">
+      <input type="submit" value="Register">
       <br>
       <br>
       <a class="btn" href="?a=login">&laquo; Back to Login Page</a>
@@ -278,34 +298,41 @@ function db_init($dbconf)
     }
 }
 
-function create_user()
+function create_user($u)
 {
-    $q = cfg('db')->prepare("
-    INSERT INTO users (user_name, user_password_hash, user_email)
-     VALUES(:user_name, :user_password_hash, :user_email)");
+  /*
+error_log(var_export($u,true));
+    $q = cfg('db')->exec("
+ INSERT INTO users (id, uid, fname, lname, email, passwd, `updated`, created)
+ VALUES ($u[id], '$u[uid]', '$u[fname]', '$u[lname]', '$u[email]', '$u[passwd]', '$u[updated]', '$u[created]')");
+error_log(var_export($q,true));
+*/
 
-    $q->bindValue(":user_name", $_POST['user_name']);
-    $q->bindValue(":user_password_hash", password_hash($_POST['user_password_new'], PASSWORD_DEFAULT));
-    $q->bindValue(":user_email", $_POST['user_email']);
+    $q = cfg('db')->prepare("
+ INSERT INTO users (id, uid, fname, lname, email, passwd, updated, created)
+ VALUES (:id, :uid, :fname, :lname, :email, :passwd, :updated, :created)");
+
+    $q->bindValue(':id', $u['id'], PDO::PARAM_INT);
+    $q->bindValue(':uid', $u['uid']);
+    $q->bindValue(':fname', $u['fname']);
+    $q->bindValue(':lname', $u['lname']);
+    $q->bindValue(':email', $u['email']);
+    $q->bindValue(':passwd', password_hash($u['passwd'], PASSWORD_DEFAULT));
+    $q->bindValue(':updated', date('Y-m-d H:i:s'));
+    $q->bindValue(':created', date('Y-m-d H:i:s'));
     if (!$q->execute()) throw new Exception(die($q->errorInfo()));
     $q->closeCursor();
 }
 
-function read_user($user)
+function read_user($uid)
 {
     return cfg('db')->query("
- SELECT user_name, user_email, user_password_hash
+ SELECT *
    FROM users
-  WHERE user_name = '$user'")->fetch(PDO::FETCH_ASSOC);
+  WHERE id = '$uid'
+     OR uid = '$uid'
+     OR email = '$uid'")->fetch(PDO::FETCH_ASSOC);
 }
 
 function update_user() {}
 function delete_user() {}
-
-function create_session($user)
-{
-    $_SESSION['user_name'] = $user['user_name'];
-    $_SESSION['user_email'] = $user['user_email'];
-    $_SESSION['user_logged_in'] = 1;
-    $_SESSION['msg'] = $user['user_name'].' is now logged in';
-}
